7a966d6cc1991132eae23738b4c6d213431fcf62
Rocky Wu <rockywu971@gmail.com>
2022-11-17 14:00:33 +0800

    test2

--- app.py
+++ app.py
@@ -207,5 +207,5 @@ def categories():
 
 
 if __name__ == "__main__": 
-  app.run(port=3000,debug=True)
-  # app.run(host = "0.0.0.0", port=3000,debug=True)
+  # app.run(port=3000,debug=True)
+  app.run(host = "0.0.0.0", port=3000,debug=True)
