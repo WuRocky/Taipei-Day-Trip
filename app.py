b3c3a953cfe8a1cdfa4e1c0950d92270e1c322f3
Rocky Wu <rockywu971@gmail.com>
2022-11-17 22:02:23 +0800

    Complete the version and upload it to EC2

--- app.py
+++ app.py
@@ -203,8 +203,6 @@ def categories():
     mycursor.close()
     connection.close()
 
-
-
 if __name__ == "__main__": 
   # app.run(port=3000,debug=True)
   app.run(host = "0.0.0.0", port=3000,debug=True)
