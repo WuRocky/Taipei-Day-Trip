445fe53a3d394062660a22cdc2338a74a4d165ba
Rocky Wu <rockywu971@gmail.com>
2022-11-20 10:42:39 +0800

    (1) attractions_id_API fetchone data from databases ，change the list of data into a dictionary

--- app.py
+++ app.py
@@ -131,39 +131,33 @@ def attractionId(attractionId):
 
     # find the corresponding id data in the database
     mycursor.execute("select * from data where id = %s",(attractionId,))
-    sql_data = mycursor.fetchall()
-    
+    api = mycursor.fetchone()
 
-    sql_data_nextPage = []
-    change_data = {}
-    for api in sql_data:
+    # get everyone data row name and data information
+    attractions_id_API = {
+      "id":api["id"],
+      "name":api["name"],
+      "category":api["category"],
+      "description":api["description"],
+      "address":api["address"],
+      "transport":api["transport"],
+      "mrt":api["mrt"],
+      "lat":api["lat"],
+      "lng":api["lng"],
+      "images":json.loads(api["images"])
+    }
 
-      # everyone data information
-      change_data = {
-        "id":api["id"],
-        "name":api["name"],
-        "category":api["category"],
-        "description":api["description"],
-        "address":api["address"],
-        "transport":api["transport"],
-        "mrt":api["mrt"],
-        "lat":api["lat"],
-        "lng":api["lng"],
-        "images":json.loads(api["images"])
-      }
-      sql_data_nextPage.append(change_data)
-    
-    # if database not data display error
-    if sql_data_nextPage == []:
+    if attractions_id_API == None:
       data_id_api_error={
       "error": True,
       "message": "請按照情境提供對應的錯誤訊息"
       }
+
       return jsonify(data_id_api_error)
 
     # show pairing info
     data_id_api= {
-      "data":sql_data_nextPage
+      "data":attractions_id_API
     }
     return jsonify(data_id_api)
   except: 
