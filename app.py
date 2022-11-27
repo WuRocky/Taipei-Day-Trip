f5e0d17d970d4bc1a9313a12ecaa34e8ccef1591
Rocky Wu <rockywu971@gmail.com>
2022-11-27 13:28:37 +0800

    change porit

--- app.py
+++ app.py
@@ -220,6 +220,6 @@ def categories():
 
 if __name__ == "__main__": 
   # app.run(port=3000,debug=True)
-  app.run(port=3000)
+  # app.run(port=3000)
   # app.run(host = "0.0.0.0", port=3000,debug=True)
-  # app.run(host = "0.0.0.0", port=3000)
+  app.run(host = "0.0.0.0", port=3000)
