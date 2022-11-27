10b77759c167a84e59f086107e01af3542dcdab4
Rocky Wu <rockywu971@gmail.com>
2022-11-27 13:15:15 +0800

    delete debug=True

--- app.py
+++ app.py
@@ -220,4 +220,5 @@ def categories():
 
 if __name__ == "__main__": 
   # app.run(port=3000,debug=True)
-  app.run(host = "0.0.0.0", port=3000,debug=True)
+  # app.run(host = "0.0.0.0", port=3000,debug=True)
+  app.run(host = "0.0.0.0", port=3000)
